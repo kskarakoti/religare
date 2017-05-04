096bb245f0e4e9652f7bb7ed72758a908891d7fd
Kirti singh <kirti@thinkdesign.in>
2017-05-04 15:36:23 +0530

    home page menu responsive

--- dist/assets/js/core.js
+++ dist/assets/js/core.js
@@ -50,25 +50,62 @@ $(function () {
 			$(".menu-lt-ar").removeClass('active');
 			$(".overlay-bg").removeClass('active');
 			$("body").removeClass('overflownone');
+			$(".hamburger li").removeClass();
+			$(".menue").removeClass('active');
 		}
 	});
 
-	//menu black background show function
-	$('.hamburg-menu-btn').click(function() {
-		$('.menu-lt-ar').toggleClass('active');
-		$(".overlay-bg").toggleClass('active');
-		$("body").toggleClass('overflownone');
+
+
+	//(Custom) - Hamburger fucntion on the left top side menu
+	var breadtop = $(".hamburger li:nth-child(1)"),
+		beef = $(".hamburger li:nth-child(2)"),
+		breadbottom = $(".hamburger li:nth-child(3)");
+	$(".menue").on('click', function() {
+		if (beef.hasClass("rot-45deg")) {
+			breadtop.removeClass("rot45deg");
+			beef.removeClass("rot-45deg");
+			breadbottom.removeClass("hidden");
+			$(".menu-lt-ar").removeClass('active');
+			$("body").removeClass('overflownone');
+			$(".overlay-bg").removeClass('active');
+			$(this).removeClass('active');
+		} else {
+			breadbottom.addClass("hidden");
+			breadtop.addClass("rot45deg");
+			beef.addClass("rot-45deg");
+			$(".menu-lt-ar").addClass('active');
+			$("body").addClass('overflownone');
+			$(".overlay-bg").addClass('active');
+			$(this).addClass('active');
+		}
 	});
 
 
 
 
 
-	if ($(window).width() < 1005) {
-		$('.sub-item').click(function() {
-			$(this).children('.sub-menu-ar').toggleClass('active');
+	// if ($(window).width() < 1005) {
+	// 	$('.sub-item').click(function() {
+	// 		$(this).children('.sub-menu-ar').toggleClass('active');
+	// 	});
+	// }
+
+	if ($(window).width() < 991) {
+		//accordion function
+		$('.sub-menu-dt-ar').hide();
+		$('.menu-list-dt > li > a').click(function() {
+			$('.menu-list-dt > li > a').parent().removeClass('active');
+			$('.sub-menu-dt-ar').slideUp('normal');
+			if($(this).next().is(':hidden') == true) {
+				//ADD THE ON CLASS TO THE BUTTON
+				$(this).parent().addClass('active');
+				//OPEN THE SLIDE
+				$(this).next().slideDown('normal');
+			}
 		});
-	}
+	};
+
 
 	//select
 	$("select").msDropdown({roundedBorder:false});
